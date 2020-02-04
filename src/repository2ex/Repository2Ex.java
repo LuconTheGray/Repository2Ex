5a98d268ada98d19c71d89036f97f58e7c50e022
LuconTheGray <johngower316@gmail.com>
2020-02-04 17:12:19 -0500

    this is the first comment

---
+++ src/repository2ex/Repository2Ex.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repository2ex;
+
+/**
+ *
+ * @author johng
+ */
+public class Repository2Ex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        YEET();
+    }
+    public static void YEET(){
+        System.out.println("YEET YEET YEEEEEEEEEEEET!!");
+    }
+    
+}
